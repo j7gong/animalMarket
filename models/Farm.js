a7be543172a74e0af5cf0f73a3e059c7343a9491
Jingsi <gongjingsi@hotmail.com>
2023-07-11 21:26:53 -0400

    set up farm model

---
+++ models/Farm.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our Farm model
+class Farm extends Model {}
+
+    // create fields/columns for Farm model
+    Farm.init(
+        {
+        // define an id column
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        // define a farmname column
+        farm_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        // define a fund column
+        fund: {
+            type: DataTypes.INTEGER,
+            defaultValue: 10000,
+            allowNull: false,
+            validate:{
+                min: 0
+            }
+        },
+        // define a user_id column that is referenced to id in user table
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+            model: 'user',
+            key: 'id'
+            }
+        }
+        },
+        {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'farm',
+        }
+    );
+  
+  module.exports = Farm;
+  
